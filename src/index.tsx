44e97cdd06ccb3be4255688ebe7114ab24edb5c8
krowter <krishna.rowter@gmail.com>
2021-04-20 10:51:35 +0700

    setup theme and stylings

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled, { ThemeProvider } from "styled-components";
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import * as theme from "./styles/theme";
+import "./styles/globals.css";
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+export const fontSize = {
+  smaller: "8px",
+  small: "12px",
+  base: "14px",
+  big: "16px",
+  bigger: "20px",
+};
+
+export const spacer = {
+  2: "4px",
+  4: "8px",
+  6: "16px",
+  8: "32px",
+  10: "40px",
+  12: "48px",
+  14: "56px",
+  16: "64px",
+  18: "72px",
+  20: "80px",
+};
+
+export const color = {
+  primary: "#f9234a",
+  secondary: "#a23550",
+  darkgrey: "#424749",
+  grey: "#6e7679",
+  lightgrey: "#e2e4e4",
+  light: "#f1f1f2",
+};
+
+export const shadow = { base: "0 8px 10px 0 rgb(10, 31, 68, 0.1)" };
